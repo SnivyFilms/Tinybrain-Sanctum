974ff8b900c3c62e797ce8f517d04f4753ec16b2
Snivy Films <Snivy.Films@gmail.com>
2026-05-29 17:46:36 -0400

    Whoops

--- EveryoneVsScps/EveryoneVsScps/Plugin.cs
+++ EveryoneVsScps/EveryoneVsScps/Plugin.cs
@@ -8,8 +8,8 @@ namespace EveryoneVsScps
     public class Plugin : Plugin<Config>
     {
         public static Plugin Instance;
-        public override string Name { get; } = "Custom Lights";
-        public override string Prefix { get; } = "CustomLights";
+        public override string Name { get; } = "EveryoneVsScps";
+        public override string Prefix { get; } = "EveryoneVsScps";
         public override string Author { get; } = "Vicious Vikki";
         public override Version Version { get; } = new Version(1, 0, 3);
         public override Version RequiredExiledVersion { get; } = new Version(9, 13, 3);
